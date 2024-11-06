8f6d71d319fa8cf2d18abc759e023d4c32a0f82a
rodolfo <00082421@uca.edu.sv>
2024-11-06 08:05:05 -0600

    :sparkles: add join SQL

---
+++ 05_join/04_union.sql
@@ -0,0 +1,26 @@
+
+-- UNION elimina duplicados
+-- como tal ful join no existe en mysql, se puede simular con union
+-- Obtiene todos los id de usuarios de las tablas dni y usuarios (exista o no relación)
+SELECT users.user_id AS u_user_id, dni.user_id AS d_user_id
+FROM users
+LEFT JOIN dni
+ON users.user_id = dni.user_id
+UNION
+SELECT users.user_id AS user_id, dni.user_id AS d_user_id
+FROM users
+RIGHT JOIN dni
+ON users.user_id = dni.user_id;
+
+-- Obtiene todos los datos de las tablas dni y usuarios (exista o no relación)
+SELECT *
+FROM users
+LEFT JOIN dni
+ON users.user_id = dni.user_id
+UNION
+SELECT *
+FROM users
+RIGHT JOIN dni
+ON users.user_id = dni.user_id;
+
+-- UNION ALL mantiene duplicados
